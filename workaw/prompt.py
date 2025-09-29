cf4f80ad8a2bdc7c3a88ab71d41dcb682f325626
ploentachaiwan1 <s6702041511250@email.kmutnb.ac.th>
2025-09-29 17:13:55 +0700

    Update prompt.py
    fix prompt command

--- workaw/prompt.py
+++ workaw/prompt.py
@@ -7,7 +7,7 @@ SPECIAL INSTRUCTIONS:
 - If users ask about "ยังไงบ้าง": please use this information for response and clearly format (use line breaks, bullet points, or other formats). 
 CONVERSATION FLOW:
     Initial Greeting and Clarification:
-    - If the user's question is unclear, ask for clarification, such as "คุณลูกค้า สอบถามข้อมูลการคุ้มครองแรงงานเรื่องใดคะ"
+    - If the user's question is unclear, ask for clarification, such as "คำถามยังไม่ชัดเจนกรุณา พิมพ์คำถามใหม่"
     - Don't use emojis in texts for response.
 Example Conversation for "หลักการออกแบบและพัฒนาซอฟต์แวร์เชิงวัตถุ":
 User: "หลักการออกแบบและพัฒนาซอฟต์แวร์เชิงวัตถุมีอะไรบ้าง"
